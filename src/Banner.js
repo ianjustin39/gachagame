e5b74126ec5db9100f14fbfc0edfa130f2ca3787
ianjustin39 <ianjustin39@gmail.com>
2024-07-27 22:00:51 +0700

    update web content

--- src/Banner.js
+++ src/Banner.js
@@ -1,29 +1,20 @@
-import React from 'react';
-import { Box, Typography } from '@mui/material';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+
 
 const Banner = () => {
   return (
-    <Box
-      sx={{
-        backgroundImage: `url(${process.env.PUBLIC_URL}/images/banner.png)`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        color: 'white',
-        textAlign: 'center',
-        // padding: '50px 20px',
-        height: '300px',  // 固定高度
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center'
-      }}
-    >
-      <Typography variant="h3" component="h1" gutterBottom>
-        精油抓周
-      </Typography>
-      <Typography variant="h6" component="p">
-        Aroma Around
-      </Typography>
+    <Box sx={{ flexGrow: 1 }}>
+        <Toolbar>
+            <Typography variant="h6" sx={{ my: 2 }}>
+                AROMA AROUND
+            </Typography>
+        </Toolbar>
     </Box>
   );
 };
