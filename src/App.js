eb9114ff84b40da90f973dbf053f1bf2af641635
ianjustin39 <ianjustin39@gmail.com>
2024-07-28 13:58:52 +0700

    update

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import Button from '@mui/material/Button';
 import ResultCard from './ResultCard';
 import Banner from './Banner';
-import Divider from '@mui/material/Divider';
+import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Unstable_Grid2';
 
 const App = () => {
   const totalCards = 45;
   const cardsData = [];
   const cardsTitle = ['1 最近看待事情的態度', '2 最近發生的困難點', '3 植物想告訴你的話'];
+  const [currentPage, setCurrentPage] = useState('init'); // 'init', 'draw', 'result'
+
 
   for (let i = 1; i <= totalCards; i++) {
     cardsData.push({ imgSrc: `${process.env.PUBLIC_URL}/images/${i}.png` });
@@ -48,26 +50,35 @@ const App = () => {
           setIsShuffling(true);
           setTimeout(() => {
             setViewResults(true);
+            setCurrentPage('result');
           }, 1000);
         }
       }, 1000); // 等待翻转动画完成后显示在空白卡片位置
     }
   };
-
   const shuffleCards = () => {
     setIsShuffling(true);
+    // 首先将所有卡片移动到中间
     const newCards = cards.map(card => ({ ...card, flipped: false }));
-    setCards(newCards);
-    setSelectedCards([]);
-    setViewResults(false);
-    setShowBlankCards([false, false, false]);
-    setIsShuffling(false);
+    setCards(newCards.map(card => ({ ...card, shuffling: true })));
+  
     setTimeout(() => {
-      setCards(shuffleArray(newCards));
-      setFlippedCount(0);
-      setIsShuffling(false);
-    }, 600);
+      // 展示所有卡片在中间聚集的效果
+      setCards(newCards.map(card => ({ ...card, transform: `translateX(${(window.innerWidth / 2) - (card.width / 2)}px)` })));
+  
+      setTimeout(() => {
+        // 洗牌并将卡片展开到新位置
+        const shuffledCards = shuffleArray(newCards);
+        setCards(shuffledCards.map(card => ({ ...card, shuffling: false, transform: '' })));
+        setSelectedCards([]);
+        setViewResults(false);
+        setShowBlankCards([false, false, false]);
+        setIsShuffling(false);
+      }, 600); // 洗牌后展开的延迟时间
+    }, 600); // 聚集到中间的持续时间
   };
+  
+  
 
   const shuffleArray = (array) => {
     return array.sort(() => Math.random() - 0.5);
@@ -77,30 +88,50 @@ const App = () => {
     shuffleCards();
   }, []);
 
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768); // 假定768px以下为手机尺寸
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <div className="App">
       <Banner />
       <Grid container spacing={2} sx={{ marginTop: '20px', marginBottom: '20px' }}>
-        <Grid xs={12}>
+        {/* <Grid xs={12}>
           {viewResults ? (
             <Button variant="contained" onClick={shuffleCards}>重新抽卡</Button>
           ) : (
             <Button onClick={shuffleCards} disabled={isShuffling} variant="contained">洗牌</Button>
           )}
-        </Grid>
+        </Grid> */}
       </Grid>
-
-      {viewResults ? (
-        <div className="result-screen">
-          <div className="selected-cards">
-            {selectedCards.map((card, index) => (
-              <div key={index} className="selected-card">
-                <ResultCard card={card} />
-              </div>
-            ))}
-          </div>
+  
+      {currentPage === 'init' && (
+        <div className="init-page">
+          <Typography sx={{ margin: 2 }}>
+            精油抓周並非占卜，而是發現自我的一個方式之一。
+          </Typography>
+          <Typography  sx={{ margin: 2 }}>
+            首先你需要先静下心来深呼吸，調整自己的心情與狀態，讓情緒處於平穩的狀態。
+          </Typography>
+          <Typography sx={{ marginBottom: 4 }}>
+            若您覺得已經準備好的，就請點擊下方『開始』按鈕，開始探索...
+          </Typography>
+          <Button variant="contained" onClick={() => [setCurrentPage('draw'),shuffleCards() ]}>
+            開始
+          </Button>
         </div>
-      ) : (
+      )}
+
+      {currentPage === 'draw' && (
         <div>
           <div className="blank-cards">
             {[...Array(3)].map((_, index) => (
@@ -121,12 +152,29 @@ const App = () => {
               </div>
             ))}
           </div>
+          <Typography  sx={{ margin: 2 }}>
+            這邊有 45 張精油卡，請您點擊洗牌後，使用您的非慣用手依序選擇三張卡。
+          </Typography>
+          <Grid container spacing={2} sx={{ marginTop: '20px', marginBottom: '20px' }}>
+            <Grid xs={12}>
+              {viewResults ? (
+                <Button variant="contained" onClick={shuffleCards}>重新抽卡</Button>
+              ) : (
+                <Button onClick={shuffleCards} disabled={isShuffling} variant="contained">洗牌</Button>
+              )}
+            </Grid>
+          </Grid>
           <div className="cards">
             {cards.map((card, index) => (
               <div
                 key={index}
-                className={`card ${card.flipped ? 'flipped' : ''} ${selectedCards.find(sc => sc.index === index) ? 'hidden' : ''}`}
+                className={`card ${card.flipped ? 'flipped' : ''} ${card.shuffling ? 'shuffling' : ''} ${selectedCards.find(sc => sc.index === index) ? 'hidden' : ''}`}
                 onClick={() => flipCard(index)}
+                style={{
+                    transform: card.shuffling ? (isMobile ? '' : `translateX(calc(${(index - 23) * 19}px))`) : '',
+                    transition: card.shuffling ? (isMobile ? 'opacity 0.6s ease' : '') : '',
+                    opacity:  card.shuffling ?(isMobile ? 0 : 1) : '',
+                }}
               >
                 <div className="card-inner">
                   <div className="card-front"></div>
@@ -139,6 +187,21 @@ const App = () => {
           </div>
         </div>
       )}
+      
+      {currentPage === 'result' && (
+        <div className="result-screen">
+          <Typography variant='h5' sx={{ margin: 2 }}>
+            您的精油卡
+          </Typography>
+          <div className="selected-cards">
+            {selectedCards.map((card, index) => (
+              <div key={index} className="selected-card">
+                <ResultCard card={card} />
+              </div>
+            ))}
+          </div>
+        </div>
+    )}  
     </div>
   );
 };
